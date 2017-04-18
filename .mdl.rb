8c7d17774204e82275682dd37d65111ea710cf79
Pierre Lavaux <pierre@lavaux.lv>
2017-04-18 15:36:53 +0200

    Add Markdown Lint config.

---
+++ .mdl.rb
@@ -0,0 +1,16 @@
+# Markdown style preferences for mdl
+# https://github.com/mivok/markdownlint/blob/master/docs/creating_styles.md
+
+# Start by including all rules
+all
+
+# Line length
+exclude_rule 'MD013'
+
+# Dollar signs used before commands without showing output
+exclude_rule 'MD014'
+
+rule 'MD029', :style => 'ordered'
+
+# Emphasis used instead of a header
+exclude_rule 'MD036'
